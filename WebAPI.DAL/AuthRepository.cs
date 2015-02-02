d54279f8c97e33a497f7e7933824e87655fb0b06
Dale Pepper <dale.pepper@tungsten-network.com>
2015-02-02 18:01:25 +0000

    Adding AuthContext and connection

---
+++ WebAPI.DAL/AuthRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using WebAPI.Models;
+
+namespace WebAPI.DAL
+{
+    class AuthRepository : IDisposable
+    {
+        private AuthContext _ctx;
+        private UserManager<IdentityUser> _userManager; 
+
+        public AuthRepository()
+        {
+            _ctx = new AuthContext();
+            _userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(_ctx));
+        }
+
+        public async Task<IdentityResult> RegisterUser(User userModel)
+        {
+            var user = new IdentityUser()
+            {
+                UserName = userModel.UserName
+            };
+
+            var result = await _userManager.CreateAsync(user, userModel.Password);
+
+            return result;
+        } 
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
